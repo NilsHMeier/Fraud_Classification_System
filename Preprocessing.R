43993d14bf37cd86ff4fa0746da1a8cf004a3bdc
NilsHMeier <nils-hendrik.meier@gmx.de>
2020-12-22 13:47:00 +0100

    Implement preprocessing

---
+++ Preprocessing.R
@@ -0,0 +1,12 @@
+# Import libraries
+library(caret)
+library(RANN)
+
+prepare_preprocessing <- function(data, methods = c("knnImpute", "center", "scale"), bounds = c(0,1), k = 15) {
+  preProcessor = preProcess(data, method=methods, rangeBounds=bounds, k=k)
+  preProcessor
+}
+
+run_preprocessing <- function(preProcessor, new_data) {
+  predict(preProcessor, newdata=new_data)
+}
